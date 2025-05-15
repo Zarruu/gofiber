57d8d28f3977070f1a837e51a9c41dfa300a707a
Zarruu <nizarakmal8@gmail.com>
2025-05-15 23:26:43 +0700

    nambah frontend

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// Sajikan file statis dari folder public
+	app.Static("/", "./public")
 
+	// Jalankan server di port 3000
 	app.Listen(":3000")
 }
