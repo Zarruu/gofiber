6c1505145b9dad1c67a57306a5cc3b29c369659f
Zarruu <nizarakmal8@gmail.com>
2025-05-22 22:01:56 +0700

    update css n js

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,19 @@ function toggleMenu() {
     const navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("show");
 }
+
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// Tutup modal saat klik di luar kotak
+window.onclick = function(event) {
+  const modal = document.getElementById("modal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
